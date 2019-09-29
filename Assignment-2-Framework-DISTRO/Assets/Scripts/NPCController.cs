774a4e2b3e19749210b6b95810c8d8c7f8266ef2
gav228 <gavrilrt@gmail.com>
2019-09-28 22:35:38 -0400

    Added project to repository

---
+++ Assignment-2-Framework-DISTRO/Assets/Scripts/NPCController.cs
@@ -0,0 +1,233 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPCController : MonoBehaviour {
+
+
+    // Store variables for objects
+    private SteeringBehavior ai;    // Put all the brains for steering in its own module
+    private Rigidbody rb;           // You'll need this for dynamic steering
+
+    // For speed 
+    public Vector3 position;        // local pointer to the RigidBody's Location vector
+    public Vector3 velocity;        // Will be needed for dynamic steering
+
+    // For rotation
+    public float orientation;       // scalar float for agent's current orientation
+    public float rotation;          // Will be needed for dynamic steering
+
+    public float maxSpeed;          // what it says
+
+    public int phase;               // use this to control which "phase" the demo is in
+
+    private Vector3 linear;         // The resilts of the kinematic steering requested
+    private float angular;          // The resilts of the kinematic steering requested
+
+    public Text label;              // Used to displaying text nearby the agent as it moves around
+    LineRenderer line;              // Used to draw circles and other things
+
+    private void Start() {
+        ai = GetComponent<SteeringBehavior>();
+        rb = GetComponent<Rigidbody>();
+        line = GetComponent<LineRenderer>();
+        position = rb.position;
+        orientation = transform.eulerAngles.y;
+    }
+
+    /// <summary>
+    /// Depending on the phase the demo is in, have the agent do the appropriate steering.
+    /// 
+    /// </summary>
+    void FixedUpdate() {
+        switch (phase) {
+            case 1:
+
+                // Pursue
+
+                // Assign algorithm label text
+                if (label) { 
+                    label.text = name.Replace("(Clone)","") + "\nAlgorithm: Smarter Pursue"; 
+                }
+
+                // Get linear acceleration
+                linear = ai.Pursue();
+                linear = ai.WallAvoidance(linear);
+                // Get angular accelleration
+                angular = ai.Face();    
+
+                break;
+
+            case 2:
+
+                // Arrive 
+
+                // Assign algorithm label text
+                if (label) {
+                    label.text = name.Replace("(Clone)", "") + "\nAlgorithm: Smarter Arrive";
+                }
+
+                // Get linear acceleration
+                linear = ai.Arrive();
+                linear = ai.WallAvoidance(linear);
+                // Get angular accelleration
+                angular = ai.Face();
+                
+                break;
+
+            case 3:
+
+                // Evade
+
+                // Assign algorithm label text
+                if (label) {
+                    label.text = name.Replace("(Clone)", "") + "\nAlgorithm: Smarter Evade";
+                }
+
+                // Get linear acceleration
+                linear = ai.Evade();
+                linear = ai.WallAvoidance(linear);
+                // Get angular acceleration
+                angular = ai.Face_Where_Im_Going(linear);
+                break;
+
+            case 4:
+                if (label) {
+                    label.text = name.Replace("(Clone)", "") + "\nAlgorithm: Smarter Wander algorithm";
+                }
+
+                angular = ai.Wander(out linear);
+                
+                //linear = ai.WallAvoidance(linear);
+                //angular = ai.Face_Where_Im_Going(linear);
+
+                break;
+            case 5:
+                if (label) {
+                    label.text = name.Replace("(Clone)", "") + "\nAlgorithm: Wall Avoidance algorithm";
+                }
+
+                linear = ai.Pursue();
+                linear = ai.WallAvoidance(linear);
+                angular = ai.Face_Where_Im_Going(linear);
+                
+                // linear = ai.whatever();  -- replace with the desired calls
+                // angular = ai.whatever();
+                break;
+
+            case 6:
+                if (label)
+                {
+                    label.text = name.Replace("(Clone)", "") + "\nAlgorithm: Collision Detection algorithm";
+                }
+
+                linear = ai.Pursue();
+                linear = ai.WallAvoidance(linear);
+                angular = ai.Face_Where_Im_Going(linear);
+
+                // linear = ai.whatever();  -- replace with the desired calls
+                // angular = ai.whatever();
+                break;
+            case 7:
+                if (label)
+                {
+                    label.text = name.Replace("(Clone)", "") + "\nAlgorithm: Collision Prediction algorithm";
+                }
+
+                linear = ai.Pursue();
+                linear = ai.WallAvoidance(linear);
+                angular = ai.Face_Where_Im_Going(linear);
+
+                // linear = ai.whatever();  -- replace with the desired calls
+                // angular = ai.whatever();
+                break;
+            case 8:
+                if (label)
+                {
+                    label.text = name.Replace("(Clone)", "") + "\nAlgorithm: Chase the player algorithm";
+                }
+
+                linear = ai.ChasePlayer();
+                linear = ai.WallAvoidance(linear);
+                angular = ai.Face_Where_Im_Going(linear);
+
+                // linear = ai.whatever();  -- replace with the desired calls
+                // angular = ai.whatever();
+                break;
+
+        }
+        update(linear, angular, Time.deltaTime);
+        if (label) {
+            label.transform.position = Camera.main.WorldToScreenPoint(this.transform.position);
+        }
+    }
+
+    private void update(Vector3 steeringlin, float steeringang, float time) {
+        // Update the orientation, velocity and rotation
+        orientation += rotation * time;
+        velocity += steeringlin * time;
+        rotation += steeringang * time;
+
+        if (velocity.magnitude > maxSpeed) {
+            velocity.Normalize();
+            velocity *= maxSpeed;
+        }
+
+        rb.AddForce(velocity - rb.velocity, ForceMode.VelocityChange);
+        position = rb.position;
+        rb.MoveRotation(Quaternion.Euler(new Vector3(0, Mathf.Rad2Deg * orientation, 0)));
+    }
+
+    // <summary>
+    // The next two methods are used to draw circles in various places as part of demoing the
+    // algorithms.
+
+    /// <summary>
+    /// Draws a circle with passed-in radius around the center point of the NPC itself.
+    /// </summary>
+    /// <param name="radius">Desired radius of the concentric circle</param>
+    public void DrawConcentricCircle(float radius) {
+        line.positionCount = 51;
+        line.useWorldSpace = false;
+        float x;
+        float z;
+        float angle = 20f;
+
+        for (int i = 0; i < 51; i++) {
+            x = Mathf.Sin(Mathf.Deg2Rad * angle) * radius;
+            z = Mathf.Cos(Mathf.Deg2Rad * angle) * radius;
+
+            line.SetPosition(i, new Vector3(x, 0, z));
+            angle += (360f / 51);
+        }
+    }
+
+    /// <summary>
+    /// Draws a circle with passed-in radius and arbitrary position relative to center of
+    /// the NPC.
+    /// </summary>
+    /// <param name="position">position relative to the center point of the NPC</param>
+    /// <param name="radius">>Desired radius of the circle</param>
+    public void DrawCircle(Vector3 position, float radius) {
+        line.positionCount = 51;
+        line.useWorldSpace = true;
+        float x;
+        float z;
+        float angle = 20f;
+
+        for (int i = 0; i < 51; i++) {
+            x = Mathf.Sin(Mathf.Deg2Rad * angle) * radius;
+            z = Mathf.Cos(Mathf.Deg2Rad * angle) * radius;
+
+            line.SetPosition(i, new Vector3(x, 1, z)+position);
+            angle += (360f / 51);
+        }
+    }
+
+    public void DestroyPoints() {
+        if (line) {
+            line.positionCount = 0;
+        }
+    }
+}
---
+++ Assignment-2-Framework-DISTRO/Assets/Scripts/SteeringBehavior.cs
@@ -0,0 +1,476 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This is the place to put all of the various steering behavior methods we're going
+/// to be using. Probably best to put them all here, not in NPCController.
+/// </summary>
+
+public class SteeringBehavior : MonoBehaviour {
+
+    // The agent at hand here, and whatever target it is dealing with
+    public NPCController agent;
+    public NPCController target;
+
+    public GameObject Player;
+
+    // Below are a bunch of variable declarations that will be used for the next few
+    // assignments. Only a few of them are needed for the first assignment.
+
+    // For pursue and evade functions
+    public float maxPrediction;
+    public float maxAcceleration;
+
+    // For arrive function
+    public float maxSpeed;
+    public float targetRadiusL;
+    public float slowRadiusL;
+    public float timeToTarget;
+
+    // For Face function
+    public float maxRotation;
+    public float maxAngularAcceleration;
+    public float targetRadiusA;
+    public float slowRadiusA;
+
+    // For wander function
+    public float wanderOffset;
+    public float wanderRadius;
+    public float wanderRate;
+    private float wanderOrientation;
+
+    // For wall avoidance
+    public float avoidDistance;
+    public float lookAhead;
+
+    // Holds the path to follow
+    public GameObject[] Path;
+    public int current = 0;
+
+    protected void Start() {
+        agent = GetComponent<NPCController>();
+        wanderOrientation = agent.orientation;
+        timeToTarget = 3.0f;
+        maxPrediction = 1f;
+        wanderRadius = 30f;
+        wanderOffset = 1f;
+        wanderRate = 2f;
+        maxAcceleration = 50f;
+        lookAhead = 0.1f;
+        avoidDistance = 3f;
+        maxRotation = 10f;
+    }
+
+    public float mapToRange(float rotation)
+    {
+        float rotationRad = rotation * Mathf.Deg2Rad;
+        if (rotationRad <= (-1 * Mathf.PI))
+        {
+            rotationRad += 2 * Mathf.PI; //RotationRad is negative and we want it to be positive
+        }
+        else if (rotationRad > Mathf.PI)
+        {
+            rotationRad += (-2 * Mathf.PI);
+        }
+
+        rotationRad = rotationRad - 2 * Mathf.PI * Mathf.Floor((rotationRad + Mathf.PI) / (2 * Mathf.PI));
+        return rotationRad;
+    }
+
+    public Vector3 Seek()
+    {
+        return target.position - agent.position;
+    }
+
+    public Vector3 Flee()
+    {
+        return agent.position - target.position;
+    }
+
+    public Vector3 Arrive()
+    {
+        if (target.position[0] - agent.position[0] < 1 && target.position[2] - agent.position[2] < 1)
+        {
+            agent.velocity = new Vector3(0, 0, 0);
+            return new Vector3(0, 0, 0);
+        }
+        
+
+        return (target.position - agent.position) / timeToTarget;
+    }
+
+    public Vector3 Pursue()
+    {
+        
+        Vector3 predicted_position = target.position;
+
+        // Distance to target
+        Vector3 direction = target.position - agent.position;
+        float distance = direction.sqrMagnitude;
+
+
+        // Find current speed
+        float speed = agent.velocity.sqrMagnitude;
+
+        // Create a prediction
+        float prediction = 0;
+        if (speed <= (distance / maxPrediction))
+        {
+            prediction = maxPrediction;
+        }
+        else
+        {
+            prediction = distance / speed;
+        }
+
+
+    
+        // Add the target's current speed to prediction
+        predicted_position += target.velocity * prediction;
+
+        agent.DrawCircle(predicted_position, 0.5f);
+
+        // Return vector to predicted location
+        return predicted_position - agent.position;
+    }
+
+    public Vector3 Evade()
+    {
+        
+        Vector3 predicted_position = target.position;
+
+        // Distance to target
+        Vector3 direction = target.position - agent.position;
+        float distance = direction.sqrMagnitude;
+
+
+        // Find current speed
+        float speed = agent.velocity.sqrMagnitude;
+
+        // Create Prediction
+        float prediction = 0;
+        if (speed <= (distance / maxPrediction))
+        {
+            prediction = maxPrediction;
+        }
+        else
+        {
+            prediction = distance / speed;
+        }
+
+        // Add the target's current speed to prediction
+        predicted_position += target.velocity * prediction;
+
+        agent.DrawCircle(predicted_position, 0.5f);
+
+        // Return vector away from predicted location
+        return agent.position - predicted_position;
+    }
+
+    public float Face()
+    {
+        Vector3 direction = target.position - agent.position;
+
+        // Check for a zero direction, and make no change if so
+        if (direction.magnitude == 0)
+        {
+            return 0;
+        }
+
+        // Get the naive direction to the target
+        float rotation = Mathf.Atan2(direction.x, direction.z) - agent.orientation;
+
+        // Map the result to the (0, 2pi) interval
+        while (rotation > Mathf.PI)
+        {
+            rotation -= 2 * Mathf.PI;
+        }
+        while (rotation < -Mathf.PI)
+        {
+            rotation += 2 * Mathf.PI;
+        }
+        float rotationSize = Mathf.Abs(rotation);
+
+        // Check if we are there, return no steering
+        if (rotationSize < targetRadiusA)
+        {
+            agent.rotation = 0;
+        }
+
+        // If we are outside the slowRadius, then use max rotation
+        // Otherwise calculate a scaled rotation
+        float targetRotation = (rotationSize > slowRadiusA ? maxRotation : maxRotation * rotationSize / slowRadiusA);
+
+        // The final target rotation combines speed (already in the variable) and direction
+        targetRotation *= rotation / rotationSize;
+
+        // Acceleration tries to get to the target rotation
+        float angular = targetRotation - agent.rotation;
+        angular /= timeToTarget;
+
+        // Check if the acceleration is too great
+        float angularAcceleration = Mathf.Abs(angular);
+        if (angularAcceleration > maxAngularAcceleration)
+        {
+            angular /= angularAcceleration;
+            angular *= maxAngularAcceleration;
+        }
+
+        return angular;
+    }
+
+    public float Face_Where_Im_Going(Vector3 linear)
+    {
+        Vector3 direction = linear;
+
+        // Check for a zero direction, and make no change if so
+        if (direction.magnitude == 0)
+        {
+            return 0;
+        }
+
+        // Get the naive direction to the target
+        float rotation = Mathf.Atan2(direction.x, direction.z) - agent.orientation;
+
+        // Map the result to the (0, 2pi) interval
+        while (rotation > Mathf.PI)
+        {
+            rotation -= 2 * Mathf.PI;
+        }
+        while (rotation < -Mathf.PI)
+        {
+            rotation += 2 * Mathf.PI;
+        }
+        float rotationSize = Mathf.Abs(rotation);
+
+        // Check if we are there, return no steering
+        if (rotationSize < targetRadiusA)
+        {
+            agent.rotation = 0;
+        }
+
+        // If we are outside the slowRadius, then use max rotation
+        // Otherwise calculate a scaled rotation
+        float targetRotation = (rotationSize > slowRadiusA ? maxRotation : maxRotation * rotationSize / slowRadiusA);
+
+        // The final target rotation combines speed (already in the variable) and direction
+        targetRotation *= rotation / rotationSize;
+
+        // Acceleration tries to get to the target rotation
+        float angular = targetRotation - agent.rotation;
+        angular /= timeToTarget;
+
+        // Check if the acceleration is too great
+        float angularAcceleration = Mathf.Abs(angular);
+        if (angularAcceleration > maxAngularAcceleration)
+        {
+            angular /= angularAcceleration;
+            angular *= maxAngularAcceleration;
+        }
+
+        return angular;
+    }
+
+    public float Align()
+    {
+        float rotation = Vector3.Angle(Vector3.forward, target.position) - agent.transform.eulerAngles.y;
+        print(Vector3.Angle(Vector3.forward, target.position));
+
+        rotation = mapToRange(rotation);
+        float rotationSize = Mathf.Abs(rotation);
+
+        float targetRotation = maxRotation;
+        targetRotation *= rotation / rotationSize;
+
+        return targetRotation - agent.rotation;
+
+    }
+
+    public float Wander(out Vector3 linear)
+    {
+        // Update the wander orientation
+        wanderOrientation += (Random.value - Random.value) * wanderRate;
+
+        // Calculate the combined target orientation
+        float orientation = wanderOrientation + agent.orientation;
+
+        // Calculate the center of the wander circle
+        Vector3 position = agent.position + wanderOffset * new Vector3(Mathf.Sin(agent.orientation), 0, Mathf.Cos(agent.orientation));
+        
+
+        // Calculate the target location
+        position += wanderRadius * new Vector3(Mathf.Sin(orientation), 0, Mathf.Cos(orientation));
+
+        // Work out the direction to target
+        Vector3 direction = position - agent.position;
+
+        // Check for a zero direction, and make no change if so
+        if (direction.magnitude == 0)
+        {
+            linear = Vector2.zero;
+            return 0;
+        }
+
+        // Get the naive direction to the target
+        float rotation = Mathf.Atan2(direction.x, direction.z) - agent.orientation;
+
+        // Map the result to the (0, 2pi) interval
+        while (rotation > Mathf.PI)
+        {
+            rotation -= 2 * Mathf.PI;
+        }
+        while (rotation < -Mathf.PI)
+        {
+            rotation += 2 * Mathf.PI;
+        }
+        float rotationSize = Mathf.Abs(rotation);
+
+        // Check if we are there, return no steering
+        if (rotationSize < targetRadiusA)
+        {
+            agent.rotation = 0;
+        }
+
+        // If we are outside the slowRadius, then use max rotation
+        // Otherwise calculate a scaled rotation
+        float targetRotation = (rotationSize > slowRadiusA ? maxRotation : maxRotation * rotationSize / slowRadiusA);
+
+        // The final target rotation combines speed (already in the variable) and direction
+        targetRotation *= rotation / rotationSize;
+
+        // Acceleration tries to get to the target rotation
+        float angular = targetRotation - agent.rotation;
+        angular /= timeToTarget;
+
+        // Check if the acceleration is too great
+        float angularAcceleration = Mathf.Abs(angular);
+        if (angularAcceleration > maxAngularAcceleration)
+        {
+            angular /= angularAcceleration;
+            angular *= maxAngularAcceleration;
+        }
+
+        // Now set the linear acceleration to be at full acceleration in the direction of the orientation
+        linear = maxAcceleration * new Vector3(Mathf.Sin(agent.orientation), 0, Mathf.Cos(agent.orientation));
+
+        RaycastHit hit;
+        bool collision = Physics.Raycast(agent.position, linear, out hit, 0.05f);
+
+        if (collision)
+        {
+            linear = maxAcceleration * ((hit.point + hit.normal) - agent.position);
+            
+        }
+
+
+        return angular;
+    }
+
+    public Vector3 WallAvoidance(Vector3 linear)
+    {
+        
+        // Setup collision detection rayVector
+        Vector3 rayVector = agent.velocity;
+        
+
+        RaycastHit hit;
+        bool collision = Physics.Raycast(agent.position, rayVector,out hit, lookAhead);
+
+        if (collision)
+        {
+            Debug.Log(hit.distance);
+            Vector3 target_position =  hit.point + hit.normal * avoidDistance;
+            agent.DrawCircle(target_position, 1);
+            return (target_position - agent.position) * maxAcceleration;
+        }
+
+
+        return linear;
+
+    }
+
+    public Vector3 CollisionDetection (Vector3 linear)
+    {
+
+        // Setup collision detection rayVector
+        Vector3 rayVector = agent.velocity;
+
+
+        RaycastHit hit;
+        bool collision = Physics.Raycast(agent.position, rayVector, out hit, lookAhead);
+
+        if (collision)
+        {
+            Debug.Log(hit.distance);
+            Vector3 target_position = hit.point + hit.normal * avoidDistance;
+            agent.DrawCircle(target_position, 1);
+            return (target_position - agent.position) * maxAcceleration;
+        }
+
+
+        return linear;
+
+    }
+
+    public Vector3 CollisionPrediction(Vector3 linear)
+    {
+
+        // Setup collision detection rayVector
+        Vector3 rayVector = agent.velocity;
+
+
+        RaycastHit hit;
+        bool collision = Physics.Raycast(agent.position, rayVector, out hit, lookAhead);
+
+        if (collision)
+        {
+            Debug.Log(hit.distance);
+            Vector3 target_position = hit.point + hit.normal * avoidDistance;
+            agent.DrawCircle(target_position, 1);
+            return (target_position - agent.position) * maxAcceleration;
+        }
+
+
+        return linear;
+
+    }
+
+    public Vector3 ChasePlayer()
+    {
+        Vector3 predicted_position = Player.GetComponent<Rigidbody>().position;
+
+        // Distance to target
+        Vector3 direction = Player.GetComponent<Rigidbody>().position - agent.position;
+        float distance = direction.sqrMagnitude;
+
+
+        // Find current speed
+        float speed = agent.velocity.sqrMagnitude;
+
+        // Create a prediction
+        float prediction = 0;
+        if (speed <= (distance / maxPrediction))
+        {
+            prediction = maxPrediction;
+        }
+        else
+        {
+            prediction = distance / speed;
+        }
+
+
+
+        // Add the target's current speed to prediction
+        predicted_position += Player.GetComponent<Rigidbody>().velocity * prediction;
+
+        agent.DrawCircle(predicted_position, 0.5f);
+
+        // Return vector to predicted location
+        return predicted_position - agent.position;
+    }
+
+
+    // ETC.
+
+}
